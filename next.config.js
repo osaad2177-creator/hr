bf84e03b29c3e5f5517933b3f106de746ef88cd8
osaad2177-creator <osaad2177@gmail.com>
2026-05-11 05:30:28 +0300

    Remove PWA configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
   },
-  i18n: {
-    locales: ['en', 'ar'],
-    defaultLocale: 'en',
-    localeDetection: false,
-  },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
