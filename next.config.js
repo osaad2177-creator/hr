446473b84d1eb5a79dba2f49993874e67fe24a9b
osaad2177-creator <osaad2177@gmail.com>
2026-05-26 05:19:07 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
-  },
-};
-module.exports = nextConfig;
