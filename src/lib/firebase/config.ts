14075bccc123e70d90cebdc4010d7179cf15c24f
osaad2177-creator <osaad2177@gmail.com>
2026-06-02 01:05:26 +0300

    Fix formatting in firebase config file

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,13 +5,14 @@ import { getStorage } from 'firebase/storage';
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-   apiKey: "AIzaSyD8ZQc9X0369LSP_OhFI6rSkm_Z7c3Il-I",
+  apiKey: "AIzaSyD8ZQc9X0369LSP_OhFI6rSkm_Z7c3Il-I",
   authDomain: "my-hr-92b9c.firebaseapp.com",
   projectId: "my-hr-92b9c",
   storageBucket: "my-hr-92b9c.firebasestorage.app",
   messagingSenderId: "112741774097",
   appId: "1:112741774097:web:d37189773a1036830312b0",
   measurementId: "G-LLT85TYM0K"
+};
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
